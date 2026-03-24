6e69b3d414369f8abe3fddbcebab63338cf33f33
Abhishek AK <abhishekak244@gmail.com>
2026-03-24 14:34:01 +0530

    baseurl added

--- src/api/Axiosinstance.js
+++ src/api/Axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://fitness-server-ffl7.onrender.com',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
